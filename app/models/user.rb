237f9c7f6858d2597e4f7b2a9a892d8c0ff60d6f
WeiJie Jhang <Vijay20051128@gmail.com>
2025-08-22 12:35:20 +0200

    img

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,13 @@ class User < ApplicationRecord
   has_many :chats,   dependent: :destroy
   # has_many :messages, through: :chats
 
+
 DEFAULT_PEOPLE = [
-  { name: "Elon Musk", fun_facts: "CEO mode." },
-  { name: "Jeff Bezos", fun_facts: "Prime time." },
+  { name: "Elon Musk", image: "https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg", fun_facts: "CEO mode." },
+  { name: "Jeff Bezos", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Jeff_Bezos-171025-F-PP655-236_%2839479699761%29_%28cropped%29.jpg/1200px-Jeff_Bezos-171025-F-PP655-236_%2839479699761%29_%28cropped%29.jpg", fun_facts: "Prime time." },
   { name: "J.K. Rowling", image: "https://upload.wikimedia.org/wikipedia/commons/5/5d/J._K._Rowling_2010.jpg", fun_facts: "Wizarding words." },
-  { name: "Mark Zuckerberg", fun_facts: "Socially networked." },
-  { name: "Taylor Swift", fun_facts: "Era after era." },
+  { name: "Mark Zuckerberg", image:"https://upload.wikimedia.org/wikipedia/commons/c/c4/Mark_Zuckerberg_F8_2018_Keynote_%28cropped%29.jpg", fun_facts: "Socially networked." },
+  { name: "Taylor Swift", image:"https://upload.wikimedia.org/wikipedia/commons/b/b1/Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png" ,fun_facts: "Era after era." },
   { name: "Warren Buffett", image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Warren_Buffett_KU_Visit.jpg", fun_facts: "Oracle of Omaha." }
 ].freeze
 
